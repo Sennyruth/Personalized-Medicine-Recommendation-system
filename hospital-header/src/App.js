49efc492a99f6ec73023d8589fc146ba63fc83b1
Senny254 <senny1328@gmail.com>
2025-01-28 23:39:22 +0300

    created and designed the contacts page

--- hospital-header/src/App.js
+++ hospital-header/src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './Pages/Home/Home';
 import Services from './Pages/Services/Services';
-import HealthDiagnosis from './Pages/Diagnosis/Diagnosis';
 import Doctors from './Pages/Doctors/Doctors';
+import Contact from './Pages/Contact/Contact';
 import Appointments from './Pages/Appointments/Appointments';
 import Contacts from './Pages/Contacts/Contacts';
+import HealthDiagnosis from './Pages/Diagnosis/Diagnosis';
 import Footer from './components/Footer';
 
 function App() {
@@ -20,7 +21,8 @@ function App() {
           <Route path="/health-diagnosis" element={<HealthDiagnosis />} />
           <Route path="/doctors" element={<Doctors />} />
           <Route path="/appointments" element={<Appointments />} />
-          <Route path="/contact" element={<Contacts />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/contacts" element={<Contacts />} />
         </Routes>
         <Footer />
       </div>
