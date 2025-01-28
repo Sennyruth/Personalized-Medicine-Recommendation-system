3b1e7b02c59df377e7ed0eaab4a3ef8ed2e6907d
Senny254 <senny1328@gmail.com>
2025-01-28 22:35:04 +0300

    Created and designed the about section of the home page

--- hospital-header/src/Pages/Home/Home.jsx
+++ hospital-header/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './Hero.jsx';
+import About from './About.jsx';
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <About />
     </div>
   );
 };
